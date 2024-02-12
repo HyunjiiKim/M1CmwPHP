699820b40bad1c00edefb2f60e784d1b73776210
HyunjiiKim <hjkim96727@gmail.com>
2024-02-12 12:00:34 +0100

    20240212

--- Premiere.php
+++ Premiere.php
@@ -24,8 +24,31 @@
 
 # 3rd exo
 
-        
+        echo "Saisir la valeur de a:";
+        $a = readline(); # $ doit se présenter toujours devant la valeur et l'instruction se termine avec ; 
+        echo "Saisir la valeur de b:";
+        $b = readline();
 
-?>
+        $Va = $a; #ajouter une nouvelle variable pour qu'on puisse enregistrer la valeur de a qui va effacer par la valeur de b
+        $b = $a;
+        $b = $Va;
+         
+        echo "\n a=" .$a."et b=".$b; # \n doit être à l'intérieur de double guillemets("")
+
+
+        $rouge = "rouge";
+        echo "la fleur est $a"; 
+        echo 'la fleur est $a';
+
+
+        date_default_timezone_set('Europe/Paris'); #Définir une zone temporelle 
+        $date = date("H:i"); #l'heure courant 
+        echo "Il est $date"."gmt.";
+
+
+        define("NOM_De_LA_CONSTANTE",Valeur);
+        #Example,
+        define("pi",3.14);
+?>  
 
 
