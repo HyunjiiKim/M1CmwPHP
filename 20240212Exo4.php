ef9d639f82be5772ae70d688f426560185c5de44
HyunjiiKim <hjkim96727@gmail.com>
2024-02-12 13:45:52 +0100

    20240212-3

---
+++ 20240212Exo4.php
@@ -0,0 +1,13 @@
+<?php
+
+    $a = 6;
+    $b = 3;
+    $add = $a+$b;
+    $sub = $a-$b;
+    $multi = $a*$b;
+    $div = $a/$b;
+    echo "$a * $b = ".$multi ."\n";
+    echo "$a / $b = ".$div ."\n"; 
+    echo "$a - $b = ".$sub ."\n";
+    echo "$a + $b = ".$add ."\n";
+?>
