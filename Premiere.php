0467270b2612e838eea948fcee9331086b5144f8
HyunjiiKim <hjkim96727@gmail.com>
2024-02-12 11:02:23 +0100

    matin

---
+++ Premiere.php
@@ -0,0 +1,31 @@
+<?php
+
+    //Saisir de nombre au clavier//
+        echo "Saisissez 2 nombres :"; //comment1// 
+        print "Saissez 2 nombres : "; 
+        # echo() = print() 
+
+    //Calcul de la moyenne//    
+        $nb1 = readline(); 
+        $nb2 = readline();
+        $total = $nb1+ $nb2;
+
+    #Afficher du résultat    
+    echo "Le total est :" .$total;
+
+
+
+    echo "Saisissez 3 nombres";
+    $nb1 = readline();
+    $nb2 = readline();
+    $nb3 = readline();
+    $moyenne = ($nb1+$nb2+$nb3)/3;
+    echo "la moyenne est:" .$moyenne;
+
+# 3rd exo
+
+        
+
+?>
+
+
